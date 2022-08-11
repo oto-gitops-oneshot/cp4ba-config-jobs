6bf151b6793058b169e8f843d48333bb11dec0b9
Mikhail Nikitin <mouse@li-0a1a642e-f729-11eb-80f0-902e16b0c16c.ibm.com>
2022-08-11 15:59:10 +1000

    Syntax correction

--- dbs/create_db.sh
+++ dbs/create_db.sh
@@ -57,8 +57,8 @@ function seed_databases {
 
     echo "Starting database creation on pod $DB2_POD_NAME" && echo
     #oc cp $DB2_COMMANDS $DB2_POD_NAME:/tmp/$DB2_COMMANDS -c db2u
-    oc exec $DB2_POD_NAME -it -c db2u -- eval($DB_COMMANDS)
-    oc exec $DB2_POD_NAME -it -c db2u -- su - db2inst1 -c "eval($DB2_COMMANDS) &"
+    oc exec $DB2_POD_NAME -it -c db2u -- eval "$DB_COMMANDS"
+    oc exec $DB2_POD_NAME -it -c db2u -- su - db2inst1 -c "eval \"$DB2_COMMANDS\" &"
 
     echo "Database setup in progress" && echo
     echo "To list the databases, run:"
