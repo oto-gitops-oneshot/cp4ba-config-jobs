26da8ec1017acd5f26b215a74bf64fe4806a840e
langley-2 <langley.millard@ibm.com>
2022-08-19 12:35:16 +1000

    change jobspec to apply certs properly

--- predeploy-cert-config/cert_gen.sh
+++ predeploy-cert-config/cert_gen.sh
@@ -14,10 +14,11 @@ dir_path=/tmp/certs
 #mkdir -p $dir_path
 # chmod -R 777 certs
 # CA cert
+
 oc project $CP4BA_NAMESPACE 
 echo "generating ca"
 openssl req -new -newkey ec -pkeyopt ec_paramgen_curve:prime256v1 -days 36500 -nodes -x509 -subj "/CN='Global CA'" -keyout $dir_path/global-ca.key -out $dir_path/global-ca.crt
-oc create secret tls global-ca --cert=$dir_path/global-ca.crt --key=$dir_path/global-ca.key -n cp4ba
+
 # Wildcard cert
 domain=$(oc --namespace openshift-ingress-operator get ingresscontrollers -o jsonpath='{$.items[0].status.domain}')
 
@@ -28,31 +29,39 @@ openssl req -new -key $dir_path/wildcard.key -subj "/CN='Global CA'" -addext "su
 
 openssl x509 -req -days 36500 -in $dir_path/wildcard.csr -CA $dir_path/global-ca.crt -CAkey $dir_path/global-ca.key -out $dir_path/wildcard.crt
 
-global_ca_cert=`cat $dir_path/global-ca.crt`
+chmod 777 -R /tmp/certs
+
+global_ca_cert=$`cat $dir_path/global-ca.crt`
 global_ca_key=`cat $dir_path/global-ca.key`
 wildcard_cert=`cat $dir_path/wildcard.crt`
 wildcard_key=`cat $dir_path/wildcard.key`
 
-echo "kind: Secret
-apiVersion: v1
-metadata:
-  name: external-tls-secret
-  namespace: cp4ba
-stringData:
-  cert.key: "$wildcard_key"
-  cert.crt: "$wildcard_cert"
-  tls.crt:  "$wildcard_cert"
-  ca.crt:   "$global_ca_cert"
-type: Opaque"  > /tmp/external_tls_cert.yaml
-oc apply -f /tmp/external_tls_cert.yaml
+# echo "kind: Secret
+# apiVersion: v1
+# metadata:
+#   name: external-tls-secret
+#   namespace: cp4ba
+# stringData:
+#   cert.key: "$wildcard_key"
+#   cert.crt: "$wildcard_cert"
+#   tls.crt:  "$wildcard_cert"
+#   ca.crt:   "$global_ca_cert"
+# type: Opaque"  > /tmp/external_tls_cert.yaml
+# oc apply -f /tmp/external_tls_cert.yaml
+oc create secret tls global-ca --cert=$dir_path/global-ca.crt --key=$dir_path/global-ca.key -n cp4ba
 
-echo "kind: Secret
-apiVersion: v1
-metadata:
-  name: cp4ba-root-ca
-  namespace: cp4ba
-stringData:
-  tls.key: "$global_ca_key"
-  tls.crt: "$global_ca_cert"
-type: Opaque" > /tmp/cp4ba_root_ca.yaml
-oc apply -f /tmp/cp4ba_root_ca.yaml
+oc create secret generic external-tls-secret --from-literal=cert.key="$wildcard_key" --from-literal="cert.crt=$wildcard_cert" --from-literal=tls.crt="$wildcard_cert" --from-literal=ca.crt="$global_ca_cert" -n cp4ba
+
+
+oc create secret tls cp4ba-root-ca --cert=$dir_path/global-ca.crt --key=$dir_path/global-ca.key -n cp4ba
+
+# echo "kind: Secret
+# apiVersion: v1
+# metadata:
+#   name: cp4ba-root-ca
+#   namespace: cp4ba
+# stringData:
+#   tls.key: "$global_ca_key"
+#   tls.crt: "$global_ca_cert"
+# " > /tmp/cp4ba_root_ca.yaml
+# oc apply -f /tmp/cp4ba_root_ca.yaml
