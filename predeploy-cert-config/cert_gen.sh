35062c401ffd85195ffeca870b8316e2193697f1
langley-2 <langley.millard@ibm.com>
2022-08-19 15:42:07 +1000

    move chmod

--- predeploy-cert-config/cert_gen.sh
+++ predeploy-cert-config/cert_gen.sh
@@ -9,10 +9,7 @@ oc_server='https://kubernetes.default.svc'
 oc login $oc_server --token=${oc_token} --certificate-authority=${CACERT} --kubeconfig="/tmp/config"
 
 
-echo "creating certs dir"
-dir_path=certs
-mkdir -p $dir_path
-chmod -R 777 certs
+
 # CA cert
 
 oc project $CP4BA_NAMESPACE 
@@ -28,9 +25,14 @@ echo $cp4ba_root_ca_exists
 
 
 if [ "$external_tls_secret_exists" = "external-tls-secret" ] | [ "$global_ca_exists" = "global-ca" ] | [ "$cp4ba_root_ca_exists" = "cp4ba-root-ca" ]; then
-    echo "A secret already exists. Please delete external-tls-secret, global-ca and cp4ba-root-ca in the cp4ba namespace and run this job again."
+    echo "A secret already exists. Please delete external-tls-secret, global-ca and cp4ba-root-ca in the $CP4BA_NAMESPACE namespace and run this job again."
+    sleep 10 
+    exit 0
 else
-        
+    echo "creating certs dir"
+    dir_path=certificates
+    mkdir -p $dir_path
+    chmod -R 777 $dir_path
     echo "generating ca"
     openssl req -new -newkey ec -pkeyopt ec_paramgen_curve:prime256v1 -days 36500 -nodes -x509 -subj "/CN='Global CA'" -keyout $dir_path/global-ca.key -out $dir_path/global-ca.crt
 
