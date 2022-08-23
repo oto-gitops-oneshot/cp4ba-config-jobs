41ff1b6519a05cd43774da7cfdd961043f77a19d
langley-2 <langley.millard@ibm.com>
2022-08-23 17:27:08 +1000

    update joospec and add script structure

--- postdeploy-config/postdeploy-config.sh
+++ postdeploy-config/postdeploy-config.sh
@@ -1,4 +1,53 @@
-#!/bin/bash\
+#!/bin/bash
 
 # script will take a list of services as args or env variable (i.e. ZEN,IER,IER-TM) and perform the relevant jobs specified in apollo 
 # reference the trello for specific tasks relevant to this project
+source ./services.sh
+
+echo "started post deploy config"
+TOKEN_PATH=/var/run/secrets/kubernetes.io/serviceaccount
+CACERT=${TOKEN_PATH}/ca.crt
+CP4BA_NAMESPACE="cp4ba"
+
+
+# set +e so the job executes without failing and doesnt hold up future sync waves
+set +e 
+
+function postdeploy_config {
+    # postdeploy services we want to configure - 
+  echo "performing post deploy tasks for $POSTDEPLOY_CONFIG_LIST"
+  if [ -z "$POSTDEPLOY_CONFIG_LIST" ]
+  then 
+    echo "config list empty" 
+   
+  else 
+    echo $POSTDEPLOY_CONFIG_LIST
+    for item in ${POSTDEPLOY_CONFIG_LIST//,/ }
+    do 
+        case $item in
+
+            ZEN)
+                configure_zen
+                ;;
+            IER) 
+                configure_ier
+                
+                ;;
+            IER-TM) 
+                configure_ier_tm
+                
+                ;;
+            TM) 
+                configure_tm
+                ;;
+            *)
+                echo "service not yet configured. Please modify bash script or check arguments"
+                ;;
+        esac
+    done
+
+  fi
+    
+}
+
+postdeploy_config
---
+++ postdeploy-config/services.sh
@@ -0,0 +1,21 @@
+
+#!/bin/bash
+set +e
+
+
+function configure_zen { 
+    echo "Configuring Zen"
+
+}
+
+function configure_ier { 
+    echo "Configuring IER"
+}
+
+function configure_ier_tm {
+    echo "Configuring IER-TM"
+}
+
+function configure_tm { 
+    echo -n "Configuring TM"
+}
