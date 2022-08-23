8612a92723c3c6db3e2d46595e4de305ea611122
langley-2 <langley.millard@ibm.com>
2022-08-23 10:00:17 +1000

    add postdeploy job resources

---
+++ postdeploy-config/postdeploy-config.sh
@@ -0,0 +1,4 @@
+#!/bin/bash\
+
+# script will take a list of services as args or env variable (i.e. ZEN,IER,IER-TM) and perform the relevant jobs specified in apollo 
+# reference the trello for specific tasks relevant to this project
